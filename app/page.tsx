556dc0b9030ffb1e7b16bdf6b5fafe298703ddc9
vxnuaj <jv.100420@gmail.com>
2024-12-23 04:38:37 -0800

    hm

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,31 @@ export default function Page() {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false); 
 
+  const mp3Files = [
+
+    '/tunes/aboutyou.mp3',
+    '/tunes/autumn1.mp3',
+    '/tunes/autumn2.mp3',
+    '/tunes/extreme.mp3',
+    '/tunes/gwyn.mp3',
+    '/tunes/lilrascal.mp3',
+    '/tunes/moon.mp3', 
+    '/tunes/os.mp3',
+    '/tunes/paris.mp3',
+    '/tunes/soulofcinder.mp3',
+    '/tunes/spaced.mp3',
+    '/tunes/whiplash.mp3',
+
+  ]
+
+
 
   const toggleAudio = () => {
     if (audioRef.current) {
+
+      const randomFile = mp3Files[Math.floor(Math.random() * mp3Files.length)];
+      audioRef.current.src = randomFile      
+
       if (isPlaying) {
         audioRef.current.pause();
       } else {
